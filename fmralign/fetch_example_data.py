040b8651cc6372cc0981cf2082dbcecd410789db
BAZEILLE Thomas <thomas.bazeille@inria.fr>
2019-02-14 18:16:21 +0100

    "tested data fetcher, able to to fetch data from specific subjects, to be used in the example"

--- fmralign/fetch_example_data.py
+++ fmralign/fetch_example_data.py
@@ -1,25 +1,16 @@
 import os
 from nilearn.datasets.utils import _fetch_files, _get_dataset_dir
-
-'''
-Example function from nilearn to download data from osf
-'''
+import pandas as pd
 
 
-def fetch_ibc_subjects_contrasts(subjects=[1, 2], conditions='all', url='https://osf.io/dx9jn/download', data_dir=None, verbose=1):
+def fetch_ibc_subjects_contrasts(subjects, data_dir=None, verbose=1):
     """Fetch all IBC contrast maps for each of subjects.
     After downloading all relevant images that are not already cached, it returns a dataframe with all needed links.
 
     Parameters
     ----------
-    subjects : list of ints <12.
-        Subjects data to download
-    conditions: str "all" by default, int or list of str
-        Pick the conditions to fetch for each subject, all of them by default
-        If a list is provided, each str is supposed to be an existing condition to fetch
-    url: string, optional
-        Override download URL. Used for test only (or if you setup a mirror of
-        the data).
+    subjects : list of str.
+        Subjects data to download. Available strings are ['sub-01', 'sub-02', 'sub-04' ... 'sub-09', 'sub-11' ... sub-15]
     data_dir: string, optional
         Path of the data directory. Used to force data storage in a specified
         location.
@@ -28,10 +19,13 @@ def fetch_ibc_subjects_contrasts(subjects=[1, 2], conditions='all', url='https:/
 
     Returns
     -------
-    data: Bunch
-        Dictionary-like object, the interest attributes are :
-        'tmap': string, giving paths to nifti contrast maps
-        'anat': string, giving paths to normalized anatomical image
+    files : list of list of str
+        List (for every subject) of list of path (for every conditions), in ap then pa acquisition.
+    metadata_df : Pandas Dataframe
+        Table containing some metadata for each available image in the dataset, as well as their pathself.
+        NOT filtered given the subjects data downloaded
+    mask: str
+        Path to the mask to be used on the data
     Notes
     ------
     This function is a caller _fetch_files in order
@@ -41,38 +35,38 @@ def fetch_ibc_subjects_contrasts(subjects=[1, 2], conditions='all', url='https:/
     nilearn.datasets.fetch_localizer_calculation_task
     nilearn.datasets.fetch_localizer_contrasts
     """
-    # The URL can be retrieved from the nilearn account on OSF
-    """all_conditions = ["audio_left_button_press", "audio_right_button_press", "video_left_button_press",     "video_right_button_press", "horizontal_checkerboard", "vertical_checkerboard", "audio_sentence", "video_sentence", "audio_computation", "video_computation", "saccades", "rotation_hand", "rotation_side", "object_grasp", "object_orientation", "mechanistic_audio", "mechanistic_video", "triangle_mental", "triangle_random", "false_belief_audio",
-                      "false_belief_video", "speech_sound", "non_speech_sound", "face_gender", "face_control", "face_trusty", "expression_intention", "expression_gender", "expression_control", "story", "math", "mental", "random", "punishment", "reward", "left_hand", "right_hand", "left_foot", "right_foot", "tongue", "cue", "shape", "face", "relational", "match", "0back_body", "2back_body", "0back_face", "2back_face", "0back_tools", "2back_tools", "0back_place", "2back_place"]
-    subjects = ['sub-%02d' %
-                i for i in [1, 2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]]
-    """
-    opts = {'uncompress': True}
+    # The URLs can be retrieved from the nilearn account on OSF
+
+    if subjects is "all":
+        subjects = ['sub-%02d' %
+                    i for i in [1, 2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]]
     dataset_name = 'ibc'
     data_dir = _get_dataset_dir(dataset_name, data_dir=data_dir,
                                 verbose=verbose)
+    metadata_path = _fetch_files(data_dir, [('ibc_3mm_all_subjects_metadata.csv',
+                                             "https://osf.io/pcvje/download", {"uncompress": True})], verbose=verbose)
+    metadata_df = pd.DataFrame.from_csv(metadata_path[0])
+    conditions = metadata_df.condition.unique()
+    metadata_df['path'] = metadata_df['path'].str.replace(
+        'path_to_dir', data_dir)
 
-    df = _fetch_files(data_dir, ("data_parser.csv",
-                                 url, opts), verbose=verbose)
-    if conditions == 'all':
-        conditions = df.contrast.unique()
-    elif not isinstance(conditions, list):
-        raise TypeError(
-            "conditions argument should be 'all', an int or a list of strings")
-
-    df['path'] = df['path'].str.replace('path_to_dir', data_dir)
-    df[df.subject.isin(subjects)][
-        df.contrast.isin(conditions)].path.values[-1]
+    '''filter : optional metadata_df[metadata_df.subject.isin(subjects)][
+        metadata_df.contrast.isin(conditions)].path.values[-1]'''
 
     mask = _fetch_files(
-        data_dir, ("mask_ibc_gm_3mm.nii.gz", url, opts), verbose=verbose)
+        data_dir, [('gm_mask_3mm.nii.gz', "https://osf.io/yvju3/download", {"uncompress": True})], verbose=verbose)
+
+    url_keys = {"sub-01": "8z23h", "sub-02": "e9kbm", "sub-04": "qn5b6", "sub-05": "u74a3", "sub-06": "83bje", "sub-07": "43j69",
+                "sub-08": "ua8qx", "sub-09": "bxwtv", "sub-11": "3dfbv", "sub-12": "uat7d", "sub-13": "p238h", "sub-14": "prdk4", "sub-15": "sw72z"}
+
+    opts = {'uncompress': True}
     files = []
     for subject in subjects:
+        url = "https://osf.io/%s/download" % url_keys[subject]
         filenames = [(os.path.join(subject, "%s_ap.nii.gz" % condition), url, opts)
                      for condition in conditions]
         filenames.extend([(os.path.join(subject, "%s_pa.nii.gz" % condition), url, opts)
                           for condition in conditions])
         files.append(_fetch_files(data_dir, filenames, verbose=verbose))
     # question should I return subject variable that is useful to filter, subject should be int in df.
-
-    return df, mask
+    return files, metadata_df, mask
