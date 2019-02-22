39b7f4cc694e4a994fa198b51897e62444d1f5c3
BAZEILLE Thomas <thomas.bazeille@inria.fr>
2019-02-22 18:54:25 +0100

    'simplify template alignment function to keep only the baseline'

--- fmralign/template_alignment.py
+++ fmralign/template_alignment.py
@@ -1,12 +1,9 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
-import copy
 from joblib import Parallel, delayed
 from sklearn.externals.joblib import Memory
 from nilearn.image import mean_img, index_img
 from fmralign.pairwise_alignment import PairwiseAlignment
-from nilearn.input_data import NiftiMasker
-from sklearn.linear_model import LinearRegression
 
 
 def euclidian_mean_with_masking(imgs, scale_template, masker):
@@ -24,85 +21,6 @@ def euclidian_mean_with_masking(imgs, scale_template, masker):
     average_img *= scale
 
     return masker.inverse_transform(mean_img)
-
-
-def reorder_img(img, labels, masker):
-    # img is 4D
-    unique_labels, counts = np.unique(labels, return_counts=True)
-    X_list_reordered = []
-    X_list = masker.transform(img)
-    for X in X_list:
-        X_reordered = []
-        for label in unique_labels:
-            i = label == labels
-            X_reordered.append(X[i[0]])
-        X_list_reordered.append(np.hstack(X_reordered))
-    return np.vstack(X_list_reordered)
-
-
-def align_template_to_images(template, imgs, method_alignment, n_pieces, n_bags, masker, n_jobs, reg):
-    '''
-    - Still should take care of clustering method/joint arguments
-    - Be sure that alignment_methods are the same in class docs and pairwise_alignment
-    - Additional arguments are available in pairwise_alignment : perturbation=False, smoothing_fwhm=None, standardize=None, detrend=False, target_affine=None, target_shape=None, low_pass=None, high_pass=None, t_r=None, memory=Memory(cachedir=None), memory_level=0.
-
-    same for align_images_to_template
-    '''
-    deformation_operators = []
-    for img in imgs:
-        template_copy = copy.copy(template)
-        piecewise_estimator = PairwiseAlignment(n_pieces=n_pieces, alignment_method=method_alignment,
-                                                mask=masker, clustering_method='k_means', joint_clustering=False, n_bags=n_bags, n_jobs=n_jobs)
-        piecewise_estimator.fit(template_copy, img)
-        deformation_operators.append(piecewise_estimator)
-        # data_csr = sparse.csr_matrix(data)
-    return deformation_operators
-
-
-def improve_template(deformation_operators, imgs, n_bags, scale_template, masker, n_jobs):
-    from scipy import sparse
-    regressed_templates, X_list = [], []
-    X_norm = 0
-    for img in imgs:
-        X_ = masker.transform(img)
-        X_list.append(X_)
-        X_norm += np.linalg.norm(X_)
-    X_stacked = np.hstack(X_list).T
-    X_norm /= len(X_list)
-    for bag in range(n_bags):
-        R_list = []
-        for deformation_operator in deformation_operators:
-            R_list.append(deformation_operator.sparse_transform(bag))
-        R_stacked = sparse.vstack(R_list)
-        linreg = LinearRegression(copy_X=False, n_jobs=n_jobs)
-        linreg.fit(R_stacked, X_stacked)
-        del R_stacked
-        regressed_templates.append(linreg.coef_)
-    del X_stacked
-    averaged_regression = np.mean(regressed_templates, axis=0)
-    scale = 1
-    if scale_template:
-        scale = X_norm / np.linalg.norm(averaged_regression)
-    return masker.inverse_transform(scale * averaged_regression)
-
-
-def create_template_regression(imgs, method_alignment, n_pieces, n_bags, scale_template, masker, n_iter, n_jobs, reg):
-    '''create_template deformed to match images s.t. :, at each iteration :
-        compute : T minimizing sum(||RT-X||) with least squares
-        compute new alignement transform images through transform R minimizing ||RT-X|| for each img
-    '''
-    template = euclidian_mean_with_masking(imgs, scale_template, masker)
-    template_history = []
-    iter = 0
-    while iter < n_iter:
-        deformation_operators = align_template_to_images(
-            template, imgs, method_alignment, n_pieces, n_bags, masker, n_jobs, reg)
-        template = improve_template(
-            deformation_operators, imgs, n_bags, scale_template, masker, n_jobs)
-        if iter < n_iter - 1:
-            template_history.append(template)
-        iter += 1
-    return template, template_history
 
 
 def align_images_to_template(imgs, template, method_alignment, n_pieces, n_bags, masker, n_iter, n_jobs, reg):
@@ -167,47 +85,6 @@ def predict_from_template_and_mapping(template, test_index,  mapping):
     return transformed_image
 
 
-def compute_greedy_template(imgs, method_alignment, n_pieces, n_bags, masker, n_jobs):
-    """
-    Greedy algorithm from Haxby 2011 to calculate a functional template from list of arrays X
-    implementation inspired of pymvpa
-
-    Parameters
-    ----------
-    X: list of array of shape n_features, n_timeframes
-    """
-
-    # To derive a single common representational space, we
-    # first hyperalign one subject to a reference subject's
-    # representational space.
-    # Then, we hyperalign a third subject
-    # to the mean response vectors for the first 2 subjects.
-    # We then hyperalign each successive subject
-    # to the mean vectors for the previously
-    # hyperaligned subjects.
-    current_template = copy.copy(imgs[0])
-    aligned_imgs = [current_template]
-    for i in range(1, len(imgs)):
-        piecewise_estimator = PieceWiseAlignment(
-            n_pieces=n_pieces, method=method_alignment, n_bootstrap=n_bags, mask=masker, n_jobs=n_jobs)
-        piecewise_estimator.fit(imgs[i], current_template)
-        aligned_img = piecewise_estimator.transform(imgs[i])
-        current_template = euclidian_mean_with_masking(
-            [current_template, aligned_img], False, masker)
-        aligned_imgs.append(aligned_img)
-
-    realigned_imgs = []
-    for i in range(len(imgs)):
-        template_left_out_subj = euclidian_mean_with_masking(
-            [x for j, x in enumerate(aligned_imgs) if j != i], False, masker)
-        piecewise_estimator = PieceWiseAlignment(
-            n_pieces=n_pieces, method=method_alignment, n_bootstrap=n_bags, mask=masker, n_jobs=n_jobs)
-        piecewise_estimator.fit(imgs[i], template_left_out_subj)
-        realigned_imgs.append(piecewise_estimator.transform(imgs[i]))
-
-    return euclidian_mean_with_masking(realigned_imgs, False, masker)
-
-
 class TemplateAlignment(BaseEstimator, TransformerMixin):
     """
     Decompose the source and target images into source and target regions
@@ -231,7 +108,7 @@ class TemplateAlignment(BaseEstimator, TransformerMixin):
         self.verbose = verbose
         self.reg = reg
 
-    def fit(self, imgs, template_method="mean", scale_template=False, n_iter=2):
+    def fit(self, imgs, scale_template=False, n_iter=2):
         """
         Learn a template from imgs
 
@@ -251,15 +128,8 @@ class TemplateAlignment(BaseEstimator, TransformerMixin):
         -------
             self
         """
-        if template_method == "regression":
-            self.template, self.template_history = create_template_regression(
-                imgs, self.alignment_method, self.n_pieces, self.n_bags, scale_template, self.mask, n_iter, self.n_jobs, self.reg)
-        elif template_method == "greedy":
-            self.template = compute_greedy_template(
-                imgs, self.alignment_method, self.n_pieces, self.n_bags, self.mask,  self.n_jobs)
-        elif template_method == "mean":
-            self.template, self.template_history = create_template(
-                imgs, self.alignment_method, self.n_pieces, self.n_bags, scale_template, self.mask, n_iter, self.n_jobs, self.reg)
+        self.template, self.template_history = create_template(
+            imgs, self.alignment_method, self.n_pieces, self.n_bags, scale_template, self.mask, n_iter, self.n_jobs, self.reg)
 
     def transform(self, imgs, train_index, test_index):
         """
