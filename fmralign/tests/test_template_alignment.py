06389e86f9a23be0586919e48617d05baec73ec7
BAZEILLE Thomas <thomas.bazeille@inria.fr>
2019-02-25 14:31:57 +0100

    'tests specification'

--- fmralign/tests/test_template_alignment.py
+++ fmralign/tests/test_template_alignment.py
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn.utils.testing import assert_array_almost_equal
 import nibabel
 from nilearn.input_data import NiftiMasker
-from functional_alignment.template_alignment import euclidian_mean
+from fmralign.template_alignment import euclidian_mean
 
 
 def test_euclidian_mean():
@@ -34,14 +34,29 @@ def test_euclidian_mean():
         ref_template, euclidian_template.get_data())
 
 
-def test_euclidian_template_wt_masking():
-
-    pass
-
-
 def test_template_identity():
-    pass
+    img1, mask_img = random_niimg((10, 10, 5, 5))
+
+    args_list = [{'alignment_method': 'identity', 'mask': mask_img},
+                 {'alignment_method': 'identity', 'n_pieces': 3, 'mask': mask_img},
+                 {'alignment_method': 'identity', 'n_pieces': 3,
+                     'n_bags': 10, 'mask': mask_img},
+                 {'alignment_method': 'identity', 'n_pieces': 3,
+                     'n_bags': 10, 'mask': mask_img, 'n_jobs': 2}
+                 ]
+    for args in args_list:
+        start = time.time()
+        algo = TemplateAlignment(**args)
+        assert_algo_transform_almost_exactly(
+            algo, img1, img1, mask=mask_img)
+        print(time.time() - start)
+
+    # Try to input some list, check that the template is two time the reference,
+
+# THEN INPUT SOME LIST OF  ONES AND SEE IF THE TRANSFORM WILL CORRECTLY LEARN TO MULTIPLY BY 2 THE TEST images
+# APART FROM THAT
 
 
 def test_template_closer_to_target():
     pass
+# Take two random images, test that their template is closer to the mean than to any of the three for every method
