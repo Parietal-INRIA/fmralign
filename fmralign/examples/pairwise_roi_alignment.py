37f621f132c1cfa521fd41a7f030963196c67b23
BAZEILLE Thomas <thomas.bazeille@inria.fr>
2019-02-25 08:59:19 +0100

    "Adding jupyter notebook version of examples "

--- fmralign/examples/pairwise_roi_alignment.py
+++ fmralign/examples/pairwise_roi_alignment.py
@@ -48,7 +48,7 @@ plot_roi(atlas_yeo, title='Original Yeo atlas',
          cut_coords=(8, -80, 9), colorbar=True, cmap='Paired')
 
 # Select visual cortex, create a mask and resample it to the resolution of the images
-mask_visual = new_img_like(atlas, atlas.get_data() == 6)
+mask_visual = new_img_like(atlas, atlas.get_data() == 1)
 resampled_mask_visual = resample_to_img(
     mask_visual, mask, interpolation="nearest")
 
