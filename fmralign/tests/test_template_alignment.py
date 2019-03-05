5f5bb8bf8110bbeac4836e26369216c0efad3380
BAZEILLE Thomas <thomas.bazeille@inria.fr>
2019-03-05 15:53:09 +0100

    'refactoring of tests and clean imports'

--- fmralign/tests/test_template_alignment.py
+++ fmralign/tests/test_template_alignment.py
@@ -1,47 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import time
-import numpy as np
-import nibabel
-from sklearn.utils.testing import assert_array_almost_equal
-from fmralign.template_alignment import TemplateAlignment, euclidian_mean
-from fmralign.tests.utils import assert_algo_transform_almost_exactly, random_niimg, assert_model_align_better_than_identity
-from nilearn.image import index_img
+from sklearn.utils.testing import assert_array_almost_equal, assert_greater
 
-
-import numpy as np
-from sklearn.utils.testing import assert_array_almost_equal
-import nibabel
 from nilearn.input_data import NiftiMasker
-from fmralign.template_alignment import euclidian_mean
+from nilearn.image import math_img, concat_imgs, index_img
 
-# Could change np.ones by np.rand normally
-sub = np.tile(np.ones((5, 4)), (5, 5, 1, 1))
-
-sub_1 = nibabel.Nifti1Image(sub, np.eye(4))
-sub_2 = nibabel.Nifti1Image(2 * sub, np.eye(4))
-sub_3 = nibabel.Nifti1Image(3 * sub, np.eye(4))
-
-ref_template = 2 * sub
-
-mask_img = nibabel.Nifti1Image(np.ones((5, 5, 5)), np.eye(4))
-
-imgs = [sub_1, sub_2, sub_3]
-masker = NiftiMasker(mask_img=mask_img)
-masker.fit()
-
-
-def test_euclidian_mean():
-
-    # apply class
-    euclidian_template = euclidian_mean(imgs, masker)
-    assert_array_almost_equal(
-        ref_template, euclidian_template.get_data())
+from fmralign.template_alignment import TemplateAlignment, euclidian_mean
+from fmralign.tests.utils import random_niimg, zero_mean_coefficient_determination
 
 
 def test_template_identity():
-
-    from nilearn.image import math_img, concat_imgs
 
     n = 10
     im, mask_img = random_niimg((6, 5, 3))
@@ -54,15 +22,18 @@ def test_template_identity():
     masker = NiftiMasker(mask_img=mask_img)
     masker.fit()
 
+    # test euclidian mean function
+    euclidian_template = euclidian_mean(subs, masker)
+    assert_array_almost_equal(
+        ref_template.get_data(), euclidian_template.get_data())
+
     args_list = [{'alignment_method': 'identity', 'mask': masker},
                  {'alignment_method': 'identity', 'mask': masker, 'n_jobs': 2},
                  {'alignment_method': 'identity', 'n_pieces': 3, 'mask': masker},
                  {'alignment_method': 'identity', 'n_pieces': 3,
                      'n_bags': 2, 'mask': masker}
                  ]
-    import time
     for args in args_list:
-        start_time = time.time()
         algo = TemplateAlignment(**args)
         # Learning a template which is
         algo.fit(subs)
@@ -70,39 +41,41 @@ def test_template_identity():
         assert_array_almost_equal(
             ref_template.get_data(), algo.template.get_data())
         predicted_imgs = algo.transform(
-            [index_img(sub_1, range(8))], train_index=range(8), test_index=range(8, 10))
-        assert_array_almost_equal(index_img(ref_template, range(8, 10)
-                                            ).get_data(), predicted_imgs[0].get_data())
-        print(time.time() - start_time)
-
-    # Try to input some list, check that the template is two time the reference,
-    # TODO:Careful with what to input. For now subjects should be 4D Niimgs and
-    # images hence list of Niimgs.
-    # Would it work with list of lists ? Should it ?
-    # TODO careful with input less than length 5
-    # TODO
-
-# THEN INPUT SOME LIST OF  ONES AND SEE IF THE TRANSFORM WILL CORRECTLY LEARN TO MULTIPLY BY 2 THE TEST images
-# APART FROM THAT
+            [index_img(sub_1, range(8))], train_index=range(8),
+            test_index=range(8, 10))
+        ground_truth = index_img(ref_template, range(8, 10))
+        assert_array_almost_equal(
+            ground_truth.get_data(), predicted_imgs[0].get_data())
 
 
 def test_template_closer_to_target():
-    if 1:
-        pass
-    args_list = [{'alignment_method': 'diagonal', 'mask': masker},
-                 {'alignment_method': 'diagonal', 'n_pieces': 3, 'mask': masker}
-                 ]
-    for args in args_list:
-        algo = TemplateAlignment(**args)
-        # Learning a template which is
+    n_samples = 6
+
+    subject_1, mask_img = random_niimg((6, 5, 3, n_samples))
+    subject_2, _ = random_niimg((6, 5, 3, n_samples))
+
+    masker = NiftiMasker(mask_img=mask_img)
+    masker.fit()
+
+    # Calculate metric between each subject and the average
+
+    sub_1 = masker.transform(subject_1)
+    sub_2 = masker.transform(subject_2)
+    subs = [subject_1, subject_2]
+    average_img = euclidian_mean(subs, masker)
+    avg_data = masker.transform(average_img)
+    mean_distance_1 = zero_mean_coefficient_determination(sub_1, avg_data)
+    mean_distance_2 = zero_mean_coefficient_determination(sub_2, avg_data)
+
+    for alignment_method in ['permutation',  'ridge_cv', 'scaled_orthogonal',
+                             'optimal_transport', 'diagonal']:
+        algo = TemplateAlignment(
+            alignment_method=alignment_method, n_pieces=3, n_bags=2, mask=masker)
+        # Learn template
         algo.fit(subs)
-        # test template
-        assert_array_almost_equal(
-            ref_template.get_data(), algo.template.get_data())
-
-        predicted_imgs = algo.transform(
-            [sub_1], train_index=range(3), test_index=range(3, 5))
-        assert_array_almost_equal(index_img(sub_1, range(3, 5)
-                                            ).get_data(), predicted_imgs[0].get_data())
-
-# Take two random images, test that their template is closer to the mean than to any of the three for every method
+        # Assess template is closer to mean than both images
+        template_data = masker.transform(algo.template)
+        template_mean_distance = zero_mean_coefficient_determination(
+            avg_data, template_data)
+        assert_greater(template_mean_distance, mean_distance_1)
+        assert_greater(template_mean_distance, mean_distance_2)
