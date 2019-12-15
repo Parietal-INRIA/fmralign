5da526f1ec13ccf4a9d04d95c9269e6faa100463
BAZEILLE Thomas <thomas.bazeille@inria.fr>
2019-12-15 11:44:22 +0100

    reput hierarchical_kmeans in fmralign, as long as it's not merged into nilearn.Parcellations

--- fmralign/_utils.py
+++ fmralign/_utils.py
@@ -7,6 +7,12 @@ from nilearn._utils.niimg_conversions import _check_same_fov
 import nilearn
 import nibabel
 from packaging import version
+import numpy as np
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.base import TransformerMixin, ClusterMixin
+from sklearn.base import BaseEstimator
+from sklearn.utils import check_array
+from sklearn.utils.validation import check_is_fitted
 
 
 def piecewise_transform(labels, estimators, X):
@@ -34,6 +40,80 @@ def piecewise_transform(labels, estimators, X):
     return X_transform
 
 
+def _remove_empty_labels(labels):
+    '''Remove empty values label values from labels list'''
+    vals = np.unique(labels)
+    inverse_vals = - np.ones(labels.max() + 1).astype(np.int)
+    inverse_vals[vals] = np.arange(len(vals))
+    return inverse_vals[labels]
+
+
+def _hierarchical_k_means(X, n_clusters, init="k-means++", batch_size=1000,
+                          n_init=10, max_no_improvement=10, verbose=0, random_state=0):
+    """ Use a recursive k-means to cluster X
+    Parameters
+    ----------
+    X: nd array (n_samples, n_features)
+        Data to cluster
+
+    n_clusters: int,
+        The number of clusters to find.
+
+    init : {'k-means++', 'random' or an ndarray}
+        Method for initialization, defaults to 'k-means++':
+        'k-means++' : selects initial cluster centers for k-mean
+        clustering in a smart way to speed up convergence. See section
+        Notes in k_init for more details.
+        'random': choose k observations (rows) at random from data for
+        the initial centroids.
+        If an ndarray is passed, it should be of shape (n_clusters, n_features)
+        and gives the initial centers.
+
+    batch_size : int, optional, default: 100
+        Size of the mini batches. (Kmeans performed through MiniBatchKMeans)
+
+    n_init : int, default=3
+        Number of random initializations that are tried.
+        In contrast to KMeans, the algorithm is only run once, using the
+        best of the ``n_init`` initializations as measured by inertia.
+
+    max_no_improvement : int, default: 10
+        Control early stopping based on the consecutive number of mini
+        batches that does not yield an improvement on the smoothed inertia.
+        To disable convergence detection based on inertia, set
+        max_no_improvement to None.
+
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for centroid initialization and
+        random reassignment. Use an int to make the randomness deterministic.
+        See :term:`Glossary <random_state>`.
+    Returns
+    -------
+    labels : list of ints (len n_features)
+        Parcellation of features in clusters
+    """
+
+    n_big_clusters = int(np.sqrt(n_clusters))
+    mbk = MiniBatchKMeans(init=init, n_clusters=n_big_clusters, batch_size=batch_size,
+                          n_init=n_init, max_no_improvement=max_no_improvement, verbose=verbose,
+                          random_state=random_state).fit(X)
+    coarse_labels = mbk.labels_
+    fine_labels = np.zeros_like(coarse_labels)
+    q = 0
+    for i in range(n_big_clusters):
+        n_small_clusters = int(
+            n_clusters * np.sum(coarse_labels == i) * 1. / X.shape[0])
+        n_small_clusters = np.maximum(1, n_small_clusters)
+        mbk = MiniBatchKMeans(init=init, n_clusters=n_small_clusters,
+                              batch_size=batch_size, n_init=n_init,
+                              max_no_improvement=max_no_improvement, verbose=verbose,
+                              random_state=random_state).fit(X[coarse_labels == i])
+        fine_labels[coarse_labels == i] = q + mbk.labels_
+        q += n_small_clusters
+
+    return _remove_empty_labels(fine_labels)
+
+
 def _make_parcellation(imgs, clustering, n_pieces, masker, smoothing_fwhm=5, verbose=0):
     """Convenience function to use nilearn Parcellation class in our pipeline.
     It is used to find local regions of the brain in which alignment will be later applied.
@@ -47,6 +127,7 @@ def _make_parcellation(imgs, clustering, n_pieces, masker, smoothing_fwhm=5, ver
         In : {'kmeans', 'ward', 'rena'}, passed to nilearn Parcellations class.
         If you aim for speed, choose k-means (and check kmeans_smoothing_fwhm parameter)
         If you want spatially connected and/or reproducible regions use 'ward'
+        If you want balanced clusters (especially from timeseries) used 'hierarchical_kmeans'
         For 'rena', need nilearn > 0.5.2
         If 3D Niimg, image used as predefined clustering, n_pieces is ignored
     n_pieces: int
@@ -64,11 +145,20 @@ def _make_parcellation(imgs, clustering, n_pieces, masker, smoothing_fwhm=5, ver
     labels : list of ints (len n_features)
         Parcellation of features in clusters
     """
+    print(clustering)
     if type(clustering) == nibabel.nifti1.Nifti1Image:
         # check image makes suitable labels, this will return friendly error message if needed
         _check_same_fov(masker.mask_img_, clustering)
         labels_img = clustering
-    else:
+    elif clustering == "hierarchical_kmeans":
+        images_to_parcel = imgs
+        if smoothing_fwhm is not None:
+            images_to_parcel = smooth_img(imgs, smoothing_fwhm)
+        X = masker.transform(images_to_parcel)
+        labels_ = _hierarchical_k_means(
+            X.T, n_clusters=n_pieces, verbose=verbose) + 1
+        labels_img = masker.inverse_transform(labels_)
+    elif clustering in ['kmeans', 'ward', 'rena']:
         if clustering == "kmeans" and smoothing_fwhm is not None:
             images_to_parcel = smooth_img(imgs, smoothing_fwhm)
         try:
@@ -84,6 +174,10 @@ def _make_parcellation(imgs, clustering, n_pieces, masker, smoothing_fwhm=5, ver
                     method=clustering, n_parcels=n_pieces, mask=masker, verbose=verbose)
         parcellation.fit(imgs)
         labels_img = parcellation.labels_img_
+    else:
+        raise InputError(
+            ('Clustering should be "kmeans", "ward", "rena", "hierarchical_kmeans", \
+             or a 3D Niimg'))
     return _apply_mask_fmri(labels_img, masker.mask_img_).astype(int)
 
 
--- fmralign/tests/test_utils.py
+++ fmralign/tests/test_utils.py
@@ -4,9 +4,19 @@ from nilearn.input_data import NiftiMasker
 import nibabel
 import pytest
 from fmralign.tests.utils import random_niimg
-from fmralign._utils import _make_parcellation, voxelwise_correlation
+from fmralign._utils import _make_parcellation, voxelwise_correlation, _hierarchical_k_means
 import nilearn
 from packaging import version
+from nilearn._utils.data_gen import generate_fake_fmri
+
+
+def test_hierarchical_k_means():
+    X = [[10, -10, 30], [12, -8, 24]]
+    truth_labels = np.tile([0, 1, 2], 5)
+    X = np.tile(X, 5).T
+    test_labels = _hierarchical_k_means(X, 3)
+    truth_labels = np.tile([test_labels[0], test_labels[1], test_labels[2]], 5)
+    assert_array_almost_equal(test_labels, truth_labels)
 
 
 def test_make_parcellation():
@@ -19,7 +29,7 @@ def test_make_parcellation():
     labels_img = nibabel.Nifti1Image(
         np.hstack([np.ones((7, 3, 8)), 2 * np.ones((7, 3, 8))]), np.eye(4))
 
-    methods = ["kmeans", "ward", labels_img]
+    methods = ["kmeans", "ward", labels_img, "hierarchical_kmeans"]
     if version.parse(nilearn.__version__) <= version.parse("0.5.2"):
         with pytest.raises(Exception):
             assert make_parcellation(img, "rena", n_pieces, masker)
@@ -32,8 +42,6 @@ def test_make_parcellation():
     # this is an exception on the installed version on nilearn for now ReNA is not released
     # out of developper mode. Once it is ready, you'll be able to call it directly
     # with the latest version of nilearn and this test will evaluate false.
-
-    mask_img.shape
 
 
 def test_voxelwise_correlation():
