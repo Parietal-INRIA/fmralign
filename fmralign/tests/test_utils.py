fc29fa704f07b084fce5f06bff6d6e2408f46e09
BAZEILLE Thomas <thomas.bazeille@inria.fr>
2020-02-07 12:06:25 +0100

    "testing new functionalities of make_parcellation"

--- fmralign/tests/test_utils.py
+++ fmralign/tests/test_utils.py
@@ -8,6 +8,8 @@ from fmralign._utils import _make_parcellation, _hierarchical_k_means
 import nilearn
 from packaging import version
 from nilearn._utils.data_gen import generate_fake_fmri
+from itertools import product
+import warnings
 
 
 def test_hierarchical_k_means():
@@ -23,22 +25,46 @@ def test_make_parcellation():
     # make_parcellation is built on Nilearn which already has several test for its Parcellation class
     # here we test just the call of the API is right on a simple example
     img, mask_img = random_niimg((7, 6, 8, 5))
+    indexes = np.arange(img.shape[-1])
     masker = NiftiMasker(mask_img=mask_img).fit()
-    n_pieces = 2
-    # create a predefined parcellation
-    labels_img = nibabel.Nifti1Image(
-        np.hstack([np.ones((7, 3, 8)), 2 * np.ones((7, 3, 8))]), np.eye(4))
+    #
 
-    methods = ["kmeans", "ward", labels_img, "hierarchical_kmeans"]
+    methods = ["kmeans", "ward", "hierarchical_kmeans"]
+
+    # check rena only if nilearn version allow it
     if version.parse(nilearn.__version__) <= version.parse("0.5.2"):
         with pytest.raises(Exception):
-            assert make_parcellation(img, "rena", n_pieces, masker)
+            assert _make_parcellation(img, "rena", n_pieces, masker)
     else:
         methods.append("rena")
+
     for clustering_method in methods:
+        # check n_pieces = 1 gives out ones of right shape
+        assert (_make_parcellation(
+            img, indexes, clustering_method, 1, masker) == masker.transform(mask_img)).all()
+
+        # check n_pieces = 2 find right clustering
         labels = _make_parcellation(
-            img, clustering_method, n_pieces, masker)
+            img, indexes, clustering_method, 2, masker)
         assert(len(np.unique(labels)) == 2)
-    # this is an exception on the installed version on nilearn for now ReNA is not released
-    # out of developper mode. Once it is ready, you'll be able to call it directly
-    # with the latest version of nilearn and this test will evaluate false.
+
+        # check that not inputing n_pieces yields problems
+        with pytest.raises(Exception):
+            assert _make_parcellation(
+                img, indexes, clustering_method, 0, masker)
+
+    clustering = nibabel.Nifti1Image(
+        np.hstack([np.ones((7, 3, 8)), 2 * np.ones((7, 3, 8))]), np.eye(4))
+
+    # check 3D Niimg clusterings
+    for n_pieces in [0, 1, 2]:
+        labels = _make_parcellation(
+            img, indexes, clustering, n_pieces, masker)
+        assert(len(np.unique(labels)) == 2)
+
+    # check warning if a parcel is too big
+    with pytest.warns(UserWarning):
+        clustering = nibabel.Nifti1Image(
+            np.hstack([np.ones(2000), 4 * np.ones(800)]), np.eye(4))
+        _make_parcellation(
+            img, indexes, clustering_method, n_pieces, masker)
