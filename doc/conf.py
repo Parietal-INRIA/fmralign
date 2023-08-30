bf1bb83d747d32b780331eb5df302c96e3b4bdcf
Elizabeth DuPre <emd222@cornell.edu>
2023-08-30 15:03:45 -0700

    Remove nistats intersphinx

--- doc/conf.py
+++ doc/conf.py
@@ -188,7 +188,6 @@ intersphinx_mapping = {
     "sklearn": ("https://scikit-learn.org/stable/", None),
     "nibabel": ("https://nipy.org/nibabel", None),
     "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
-    "nistats": ("https://nistats.github.io", None),
     "joblib": ("https://joblib.readthedocs.io/en/latest/", None),
 }
 
