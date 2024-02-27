995f82a202f4df5648fc89bdb3b5821f52408f5f
FOUCHARD Denis <denis.fouchard@inria.fr>
2024-02-27 10:06:11 +0100

    Add toy experiment to examples

--- examples/plot_toy_int_experiment.py
+++ examples/plot_toy_int_experiment.py
@@ -1,13 +1,30 @@
-"""Toy experiment to test INT on the two parts of the data (ie different runs
-of the experiment) to acess the validity of tuning computation. This code as
-no vocation to be an explanatory example, it is only used to test the INT
-method. It is not intended to be used as a tutorial.
-"""
+# -*- coding: utf-8 -*-
 
+"""
+Co-smoothing Prediction using the IndividualNeuralTuning Model.
+See article : https://doi.org/10.1162/imag_a_00032
+
+==========================
+
+This is a toy experiment to test Individual Tuning Model (INT) on two parts of the 
+data (or different runs) to assess the validity of tuning computation. This code has
+no vocation to be an explanatory example, but rather a test to check the validity of
+the INT model.
+
+
+To run this example, you must launch IPython via ``ipython
+--matplotlib`` in a terminal, or use ``jupyter-notebook``.
+
+.. contents:: **Contents**
+    :local:
+    :depth: 1
+
+"""
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 from fmralign.alignment_methods import IndividualizedNeuralTuning as INT
-from fmralign.generate_data import generate_dummy_signal, generate_dummy_searchlights
+from fmralign.generate_data import generate_dummy_signal
 from fmralign.hyperalignment.correlation import (
     tuning_correlation,
     stimulus_correlation,
@@ -15,24 +32,28 @@ from fmralign.hyperalignment.correlation import (
     matrix_MDS,
 )
 
-
-#############################################################################
-# INT
-#############################################################################
-
+# %%
+###############################################################################
+# Generate the data
+# -----------------
+# In this example we use toy data to test the INT model. We generate two runs of
+# the experiment, and we use the INT model to align the two runs. We then compare
+# the tuning matrices and the shared response to assess the validity of the INT model.
+# We also compare the reconstructed images to the ground truth to assess the validity
+# of the INT model.
+# The toy generation function allows us to get the ground truth stimulus and tuning
+# matrices that were used to generate the data, and we can also control the level of
+# noise in the data.
 
 n_subjects = 10
 n_timepoints = 200
 n_voxels = 500
-S_std = 5
+S_std = 5  # Standard deviation of the source components
 T_std = 1
-SNR = 100
+SNR = 100  # Signal to noise ratio
 latent_dim = 15  # if None, latent_dim = n_t
 decomposition_method = "pca"  # if None, SVD is used
 
-
-#############################################################################
-# GENERATE DUMMY SIGNAL
 
 (
     data_run_1,
@@ -51,48 +72,27 @@ decomposition_method = "pca"  # if None, SVD is used
     seed=42,
 )
 
-SEARCHLIGHT = False
+parcels = [range(n_voxels)]
 
-if SEARCHLIGHT:
-    searchlights, dists = generate_dummy_searchlights(
-        n_searchlights=n_voxels, n_v=n_voxels, radius=5
-    )
-    int1 = INT(
-        n_components=latent_dim,
-        decomp_method=decomposition_method,
-        alignment_method="searchlight",
-    )
-    int2 = INT(
-        n_components=latent_dim,
-        decomp_method=decomposition_method,
-        alignment_method="searchlight",
-    )
-    int_first_part = int1.fit(
-        data_run_1, searchlights=searchlights, dists=dists, radius=5, verbose=False
-    )  # S is provided if we cheat and know the ground truth
-    int_second_part = int2.fit(
-        data_run_2, searchlights=searchlights, dists=dists, radius=5, verbose=False
-    )
-
-
-else:
-    parcels = [range(n_voxels)]
-    int1 = INT(
-        n_components=latent_dim,
-        decomp_method=decomposition_method,
-    )
-    int2 = INT(
-        n_components=latent_dim,
-        decomp_method=decomposition_method,
-    )
-    int_first_part = int1.fit(
-        data_run_1, parcels=parcels, verbose=False
-    )  # S is provided if we cheat and know the ground truth
-    int_second_part = int2.fit(data_run_2, parcels=parcels, verbose=False)
-
+# %%
 #############################################################################
-# Test INT on the two parts of the data (ie different runs of the experiment)
+# Create two independant instances of the model
+# ---------------------------------------------
+# We create two instances of the INT model to align the two runs of the experiment.
+# We then extract the tuning matrices and the shared from the two runs to compare them.
+#
+#
 
+int1 = INT(
+    n_components=latent_dim,
+    decomp_method=decomposition_method,
+)
+int2 = INT(
+    n_components=latent_dim,
+    decomp_method=decomposition_method,
+)
+int1.fit(data_run_1, parcels=parcels, verbose=False)
+int2.fit(data_run_2, parcels=parcels, verbose=False)
 
 # save individual components
 tuning_pred_run_1 = int1.tuning_data
@@ -105,19 +105,25 @@ stimulus_pred_run_2 = int2.shared_response
 
 data_pred = int1.transform(data_run_2)
 
+# %%
+###############################################################################
+# Plotting validation metrics
+# ---------------------------
+# We compare the tuning matrices and the shared response to assess the validity
+# of the INT model. To achieve that, we use Pearson correlation between true and
+# estimated stimulus, as well as between true and estimated tuning matrices.
+# For tuning matrices, this is dones by first computing the correlation between
+# every pair of tuning matrices from the two runs of the experiment, and then
+# averaging the correlation across the diagonal (ie the correlation between
+# the same timepoint of the two runs).
 
-#############################################################################
-# Plot
-#############################################################################
-
-plt.rc("font", size=6)
-fig, ax = plt.subplots(2, 3, figsize=(10, 5))
+fig, ax = plt.subplots(2, 3, figsize=(15, 8))
 
 
 # Tunning matrices
 correlation_tuning = tuning_correlation(tuning_pred_run_1, tuning_pred_run_2)
 ax[0, 0].imshow(correlation_tuning)
-ax[0, 0].set_title("Correlation Tuning Run 1 vs Run 2")
+ax[0, 0].set_title("Pearson correlation of tuning matrices (Run 1 vs Run 2)")
 ax[0, 0].set_xlabel("Subjects, Run 1")
 ax[0, 0].set_ylabel("Subjects, Run 2")
 fig.colorbar(ax[0, 0].imshow(correlation_tuning), ax=ax[0, 0])
@@ -149,7 +155,6 @@ T_first_part_transformed, T_second_part_transformed = matrix_MDS(
     tuning_pred_run_1, tuning_pred_run_2, n_components=2, dissimilarity=1 - corr_tunning
 )
 
-
 ax[0, 2].scatter(
     T_first_part_transformed[:, 0],
     T_first_part_transformed[:, 1],
@@ -163,13 +168,16 @@ ax[0, 2].scatter(
     c=random_colors,
 )
 ax[0, 2].set_title("MDS of tunning matrices, dim=2")
+# Set square aspect
+ax[0, 1].set_aspect("equal", "box")
+ax[0, 2].set_aspect("equal", "box")
 
 # Stimulus matrix correlation
 correlation_stimulus_true_est_first_part = stimulus_correlation(
     stimulus_pred_run_1.T, stimulus_run_1.T
 )
 ax[1, 0].imshow(correlation_stimulus_true_est_first_part)
-ax[1, 0].set_title("Stimumus Estimated vs ground truth (Run 1)")
+ax[1, 0].set_title("Correlation of estimated stimulus vs ground truth (Run 1)")
 ax[1, 0].set_xlabel("Latent components, Run 1")
 ax[1, 0].set_ylabel("Latent components, ground truth")
 fig.colorbar(ax[1, 0].imshow(correlation_stimulus_true_est_first_part), ax=ax[1, 0])
@@ -178,7 +186,7 @@ correlation_stimulus_true_est_second_part = stimulus_correlation(
     stimulus_pred_run_2.T, stimulus_run_2.T
 )
 ax[1, 1].imshow(correlation_stimulus_true_est_second_part)
-ax[1, 1].set_title("Stimulus Estimated vs ground truth (Run 2)")
+ax[1, 1].set_title("Correlation of estimated stimulus vs ground truth (Run 2))")
 ax[1, 1].set_xlabel("Latent components, Run 2")
 ax[1, 1].set_ylabel("Latent components, ground truth")
 fig.colorbar(ax[1, 1].imshow(correlation_stimulus_true_est_second_part), ax=ax[1, 1])
@@ -187,17 +195,20 @@ fig.colorbar(ax[1, 1].imshow(correlation_stimulus_true_est_second_part), ax=ax[1
 # Reconstruction
 corr_reconstruction = tuning_correlation(data_pred, data_run_2)
 ax[1, 2].imshow(corr_reconstruction)
-ax[1, 2].set_title("Reconstruction correlation")
+ax[1, 2].set_title("Correlation of brain response (Run 2 vs Ground truth)")
 ax[1, 2].set_xlabel("Subjects, Run 2")
-ax[1, 2].set_ylabel("Subjects, Run 1")
+ax[1, 2].set_ylabel("Subjects, Ground truth")
 fig.colorbar(ax[1, 2].imshow(corr_reconstruction), ax=ax[1, 2])
 
 
 plt.rc("font", size=10)
 # Define small font for titles
 fig.suptitle(
-    f"Correlation Run 1/2\n ns={n_subjects}, nt={n_timepoints}, nv={n_voxels}, S_std={S_std}, T_std={T_std}, SNR={SNR}, latent space dim={latent_dim}"
+    "Correlation metrics for the Individual Tuning Model\n"
+    + f"{n_subjects} subjects, {n_timepoints} timepoints, {n_voxels} voxels, {latent_dim} latent components\n"
+    + f"SNR={SNR}"
 )
-plt.tight_layout()
 
+plt.tight_layout()
+# %%
 plt.show()
