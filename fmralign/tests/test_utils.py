6094e637a0d3503cad8f5ae3cb8a8f641b38f311
Pierre-Louis Barbarant <plbarbarant@mail.com>
2024-11-10 22:50:23 +0100

    Add tests for ParceledData class and improve test_utils.py structure

--- fmralign/tests/test_utils.py
+++ fmralign/tests/test_utils.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-import nibabel
+import nibabel as nib
 import numpy as np
 import pytest
 from nilearn.maskers import NiftiMasker
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
-from fmralign._utils import _make_parcellation
-from fmralign.tests.utils import random_niimg
+from fmralign._utils import _make_parcellation, ParceledData
+from fmralign.tests.utils import random_niimg, sample_parceled_data
 
 
 def test_make_parcellation():
@@ -32,7 +33,7 @@ def test_make_parcellation():
         with pytest.raises(Exception):
             assert _make_parcellation(img, clustering_method, 0, masker)
 
-    clustering = nibabel.Nifti1Image(
+    clustering = nib.Nifti1Image(
         np.hstack([np.ones((7, 3, 8)), 2 * np.ones((7, 3, 8))]), np.eye(4)
     )
 
@@ -43,7 +44,151 @@ def test_make_parcellation():
 
     # check warning if a parcel is too big
     with pytest.warns(UserWarning):
-        clustering = nibabel.Nifti1Image(
+        clustering = nib.Nifti1Image(
             np.hstack([np.ones(2000), 4 * np.ones(800)]), np.eye(4)
         )
         _make_parcellation(img, clustering_method, n_pieces, masker)
+
+
+def test_initialization():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    assert_array_equal(parceled.data, data)
+    assert parceled.masker == masker
+    assert_array_equal(parceled.labels, labels)
+    assert_array_equal(parceled.unique_labels, [1, 2])
+    assert parceled.n_pieces == 2
+
+
+def test_getitem_single_parcel():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    # Test getting every parcel individually
+    for i in range(2):
+        parcel = parceled[i]
+        expected = data[:, labels == i + 1]
+        assert_array_equal(parcel, expected)
+
+
+def test_getitem_slice():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    # Test getting all parcels with slice
+    all_parcels = parceled[:]
+    assert len(all_parcels) == 2
+    assert_array_equal(all_parcels[0], parceled[0])
+    assert_array_equal(all_parcels[1], parceled[1])
+
+    # Test step parameter
+    stepped_parcels = parceled[::2]
+    assert len(stepped_parcels) == 1
+    assert_array_equal(stepped_parcels[0], parceled[0])
+
+
+def test_getitem_invalid_key():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    with pytest.raises(ValueError, match="Invalid key type."):
+        parceled["invalid"]
+
+
+def test_get_parcel():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    # Test getting parcel with label 1
+    parcel_0 = parceled.get_parcel(1)
+    expected = data[:, labels == 1]
+    assert_array_equal(parcel_0, expected)
+
+    # Test getting parcel with label 2
+    parcel_1 = parceled.get_parcel(2)
+    expected = data[:, labels == 2]
+    assert_array_equal(parcel_1, expected)
+
+
+def test_to_list():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    parcel_list = parceled.to_list()
+    assert len(parcel_list) == 2
+    assert_array_equal(parcel_list[0], parceled[0])
+    assert_array_equal(parcel_list[1], parceled[1])
+
+
+def test_to_img():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    img = parceled.to_img()
+    assert isinstance(img, nib.Nifti1Image)
+
+    # Test shape of the reconstructed image
+    assert img.shape == (8, 7, 6, 20)
+
+    # Test that we can transform back to original data
+    reconstructed_data = masker.transform(img)
+    assert_array_almost_equal(reconstructed_data, data)
+
+
+def test_edge_cases():
+    # Test with single parcel
+    data, masker, labels = sample_parceled_data(n_pieces=1)
+    parceled = ParceledData(data, masker, labels)
+    assert parceled.n_pieces == 1
+    assert_array_equal(parceled[0], data)
+    assert 0 == 0
+
+
+def test_out_of_bounds_access():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    with pytest.raises(IndexError):
+        parceled[2]  # Trying to access non-existent parcel
+
+    with pytest.raises(IndexError):
+        parceled[-3]  # Trying to access with invalid negative index
+
+
+def test_slice_bounds():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    parceled = ParceledData(data, masker, labels)
+
+    # # Test slices beyond bounds
+    # with pytest.raises(IndexError):
+    #     parceled[2:10]
+
+    # Test negative slices
+    result = parceled[-1:-3:-1]
+    assert len(result) == 2
+    assert_array_equal(result[1], parceled[0])
+    assert_array_equal(result[0], parceled[1])
+
+
+def test_non_contiguous_labels():
+    data, masker, labels = sample_parceled_data(n_pieces=2)
+    # Replace labels 1, 2 with 1, 3
+    labels[labels == 2] = 3
+    parceled = ParceledData(data, masker, labels)
+
+    assert_array_equal(parceled.unique_labels, [1, 3])
+    assert parceled.n_pieces == 2
+
+    # Test accessing by index
+    first_parcel = parceled[0]
+    expected = data[:, labels == 1]
+    assert_array_equal(first_parcel, expected)
+
+    # Test accessing by label
+    same_parcel = parceled.get_parcel(1)
+    assert_array_equal(same_parcel, expected)
+
+
+if __name__ == "__main__":
+    test_non_contiguous_labels()
