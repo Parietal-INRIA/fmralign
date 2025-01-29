b491cefc9eff2817bc48842e75074f6293d6857e
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-01-29 17:40:57 +0100

    Remove unused pytest import from test_alignment_methods.py

--- fmralign/tests/test_alignment_methods.py
+++ fmralign/tests/test_alignment_methods.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import pytest
 from numpy.testing import assert_array_almost_equal
 from scipy.linalg import orthogonal_procrustes
 from scipy.sparse import csc_matrix
