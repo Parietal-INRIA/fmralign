9bfb7123a74b3db5c2e75f179442ba3d2ad89b93
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-02-12 22:53:11 +0100

    Rename POTAlignment class to OptimalTransportAlignment and remove JAX dependencies from implementation

--- fmralign/alignment_methods.py
+++ fmralign/alignment_methods.py
@@ -281,7 +281,7 @@ class RidgeAlignment(Alignment):
         return self.R.predict(X)
 
 
-class POTAlignment(Alignment):
+class OptimalTransportAlignment(Alignment):
     """
     Compute the optimal coupling between X and Y with entropic regularization,
     using the pure Python POT (https://pythonot.github.io/) package.
@@ -364,75 +364,6 @@ class POTAlignment(Alignment):
     def transform(self, X):
         """Transform X using optimal coupling computed during fit."""
         return X.dot(self.R)
-
-
-class OptimalTransportAlignment(Alignment):
-    """
-    Compute the (unbalanced) optimal coupling between X and Y
-    with entropic regularization using
-    OTT-JAX as a backend for acceleration.
-
-    Parameters
-    ----------
-    metric : str(optional)
-        metric used to create transport cost matrix,
-        see full list in scipy.spatial.distance.cdist doc
-    reg : int (optional)
-        level of entropic regularization
-    tau : float (optional)
-        level of unbalancing, 1.0 corresponds to balanced transport,
-        lower values will favor lower mass transport
-
-    Attributes
-    ----------
-    R : jaxlib.xla_extension.Array
-        Mixing matrix containing the optimal permutation
-    """
-
-    def __init__(
-        self, metric="euclidean", reg=1, tau=1.0, max_iter=1000, tol=1e-3
-    ):
-        self.metric = metric
-        self.reg = reg
-        self.tau = tau
-        self.tol = tol
-        self.max_iter = max_iter
-
-    def fit(self, X, Y):
-        """
-
-        Parameters
-        ----------
-        X: (n_samples, n_features) nd array
-            source data
-        Y: (n_samples, n_features) nd array
-            target data
-        """
-        import jax
-        from ott.geometry import costs, geometry
-        from ott.problems.linear import linear_problem
-        from ott.solvers.linear import sinkhorn
-
-        if self.metric == "euclidean":
-            cost_matrix = costs.Euclidean().all_pairs(x=X.T, y=Y.T)
-        else:
-            cost_matrix = cdist(X.T, Y.T, metric=self.metric)
-
-        geom = geometry.Geometry(cost_matrix=cost_matrix, epsilon=self.reg)
-        problem = linear_problem.LinearProblem(
-            geom, tau_a=self.tau, tau_b=self.tau
-        )
-        solver = sinkhorn.Sinkhorn(
-            geom, max_iterations=self.max_iter, threshold=self.tol
-        )
-        P = jax.jit(solver)(problem)
-        self.R = np.asarray(P.matrix) * len(X.T)
-
-        return self
-
-    def transform(self, X):
-        """Transform X using optimal coupling computed during fit."""
-        return X @ self.R
 
 
 class IndividualizedNeuralTuning(Alignment):
