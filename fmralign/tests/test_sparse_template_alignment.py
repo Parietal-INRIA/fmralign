5d0b56284107e207095ed00ba857497998d788c0
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-04-08 02:24:55 +0200

    Refactor test_consistency_with_dense_templates to iterate over images for transformation checks

--- fmralign/tests/test_sparse_template_alignment.py
+++ fmralign/tests/test_sparse_template_alignment.py
@@ -142,18 +142,22 @@ def test_consistency_with_dense_templates():
     )
     sparse_algo.fit([img1, img2, img3])
 
+    # Check that the templates are the same
     template1 = dense_algo.template_img
     template2 = sparse_algo.template_img
-    img1_dense_transformed = dense_algo.transform(img1, subject_index=0)
-    img1_sparse_transformed = sparse_algo.transform(img1, subject_index=0)
-
-    # Check that the templates are the same
     assert_array_almost_equal(
         masker.transform(template1),
         masker.transform(template2),
     )
+
     # Check that the transformed images are the same
-    assert_array_almost_equal(
-        masker.transform(img1_dense_transformed),
-        masker.transform(img1_sparse_transformed),
-    )
+    for i, img in enumerate([img1, img2, img3]):
+        img_dense_transformed = dense_algo.transform(img, subject_index=i)
+        img_sparse_transformed = sparse_algo.transform(img, subject_index=i)
+        assert_array_almost_equal(
+            masker.transform(img_dense_transformed),
+            masker.transform(img_sparse_transformed),
+        )
+
+
+test_consistency_with_dense_templates()
