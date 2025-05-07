1d1e2655fc25df3d1eb48618198b7530813c04ca
Pierre-Louis Barbarant <pierre-louis.barbarant@inria.fr>
2025-05-07 16:40:52 +0200

    Remove unnecessary comment in pairwise alignment tutorial for clarity

--- examples/plot_pairwise_alignment.py
+++ examples/plot_pairwise_alignment.py
@@ -3,7 +3,7 @@
 """
 Pairwise functional alignment.
 ==============================
-This is a comment
+
 In this tutorial, we show how to better predict new contrasts for a target
 subject using source subject corresponding contrasts and data in common.
 
